48b8ec07e8852b3de9e134b70b798bf2ea5b4d99
sukriti-ksolves <sukriti.chatterjee@ksolves.com>
2021-09-08 14:56:07 +0530

    Bug Fixes and basic Tree view added

--- salesforce_modules/models/__init__.py
+++ salesforce_modules/models/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from . import users, account, geographic_scope_c, task, cnae, user_company_contract_permissions_c, lead
-
+from . import opportunity
 from . import lead_sort_columns_in_campaign, campaign, model__c
 from . import pathology, quotation_item_price, accredited_network,region__c, quote_line_item
 
