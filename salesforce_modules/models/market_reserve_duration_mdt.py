183467da7036f4af35188a5c8db030826ddff573
sukriti-ksolves <sukriti.chatterjee@ksolves.com>
2021-09-13 10:24:00 +0530

    Added New Models, views and a few bug fixing

--- salesforce_modules/models/market_reserve_duration_mdt.py
+++ salesforce_modules/models/market_reserve_duration_mdt.py
@@ -9,7 +9,7 @@ class Market_Reserve_Duration_MDT(models.Model):
     # LastModifiedById already in odoo
 
     DeveloperName = fields.Text(string='Custom Metadata Record Name',size=40)
-    MasterLabel = fields.Text(string='Label', size='40')
+    MasterLabel = fields.Text(string='Label', size=40)
     NamespacePrefix = fields.Text(string='Namespace Prefix')
     IsProtected = fields.Boolean(string='Protected Component')
     Duration = fields.Float(string='Duration',digits=(18,0))
---
+++ salesforce_modules/models/politic_bill.py
@@ -0,0 +1,21 @@
+from odoo import models, fields, api
+
+
+class SalesforcePoliticBill(models.Model):
+
+    _name = 'politic.bill'
+    _description = 'Salesforce Politic Bill'
+
+    user_id = fields.Many2one('res.users', string="Created By")
+    last_modified_by_id = fields.Many2one('res.users', string="Last Modified By")
+    life = fields.Integer('Número de Vidas',digits=(18, 0))
+    reimbursement_value = fields.Float('Reimbursement Value',digits=(3,2))
+    consultation_reimbursement = fields.Float('Consultation Reimbursement',digits=(16,2))
+    unitary_value = fields.Float('Unitary value',digits=(16,2))
+    owner_id = fields.Many2one('account.account', string='Owner')
+    plan_id = fields.Many2one('product.product', 'Plano')
+    reimbursement_value_type = fields.Selection([
+        ('Fixed', 'Fixed'),
+        ('Percentage', 'Percentage'),
+    ], string="Reimbursement Value Type")
+    rule_note = fields.Char('Rule note',size=200)
