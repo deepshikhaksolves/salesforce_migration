fa998127ae69a9aef6ede1c1f1703cfc7b5a54d2
asishkumar-ksolves <asish.nayak@ksolves.com>
2021-10-27 10:31:01 +0530

    updated

--- pit_executive_operation/__manifest__.py
+++ pit_executive_operation/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "PIT Executive: Operations",
+    'name': "PIT Executive: Operation",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
